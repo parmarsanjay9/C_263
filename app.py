d5a3212021148137f98c15203ecf8b006beaa6da
Sanjay Parmar <uttammodil123@gmail.com>
2024-03-30 15:37:58 +0530

    first commit

--- app.py
+++ app.py
@@ -1,38 +1,59 @@
-from flask import Flask, render_template, request
+#import flask library
+from flask import Flask , render_template , request
+import requests
 
+#initialize flask
 app = Flask(__name__)
 
+#route your webpage
 @app.route("/")
+
 def visitors():
 
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    # Increment the count
-    visitors_count = visitors_count + 1
+	# Increment the count
+	visitors_count = visitors_count + 1
 
-    # Overwrite the count
-    counter_write_file = open("count.txt", "w")
-    counter_write_file.write(str(visitors_count))
-    counter_write_file.close()
+	# Overwrite the count
+	counter_write_file = open("count.txt", "w")
+	counter_write_file.write(str(visitors_count))
+	counter_write_file.close()
 
-    # Render HTML with count variable
-    return render_template("index.html", count=visitors_count)
+	return render_template('index.html', count=visitors_count)
 
-@app.route('/', methods=['POST'])
+# Render HTML with count variable
+
+
+#route your webpage
+@app.route("/", methods=['POST'])
 def covid_stats():
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    text = request.form['text']
+	latitude = request.form['latitude']
+	longitude = request.form['longitude']
 
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
-    print(corona_data)
-    return render_template("index.html", image=corona_data, count=visitors_count)
+	api_url = 'https://weather-l6tl.onrender.com/api/getCurrentWeather/'+latitude+'/'+longitude
 
-if __name__ == "__main__":
-    app.run()
+	response = requests.get(api_url)
+	weather_data = response.json()
+	print(weather_data)
+
+	return render_template('index.html',weather=weather_data,count=visitors_count)
+
+
+if __name__ == '__main__':
+	app.run()
+
+
+
+
+	#complete the code
+
+#add code for executing flask
